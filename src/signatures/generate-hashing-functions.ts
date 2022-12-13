ce9b40ea76714dd35a329d60d27a74ed51340c75
Dmytro Sotnyk <d3magexgod@gmail.com>
2022-12-13 12:37:09 +0200

    hashing function generation

--- src/signatures/generate-hashing-functions.ts
+++ src/signatures/generate-hashing-functions.ts
@@ -1,5 +1,5 @@
 import { ScrappedStruct } from '../types';
-import { SOLIDITY_HEADER } from './common';
+import { SOLIDITY_HEADER } from './shared';
 
 export const generateHashingFunctions = (structs: ScrappedStruct[]): string => {
   let resultString = SOLIDITY_HEADER;
@@ -10,10 +10,42 @@ export const generateHashingFunctions = (structs: ScrappedStruct[]): string => {
   
   library RecoverSigner {`;
   for (const struct of structs) {
-    const structName = struct.structName; 
+    resultString += generateSingleFunction(struct);
   }
   resultString += `}`;
   return resultString;
 };
 
+const generateSingleFunction = (struct: ScrappedStruct): string => {
+  const structName = struct.structName;
+  console.log(struct);
+  let fieldString = '';
+  for (let i = 0; i < struct.fields.length; ++i) {
+    const field = struct.fields[i];
+    const dataType = field.dataType;
+    //todo: check all elementary types
+    if (dataType.type == 'ElementaryTypeName') {
+      if (dataType.name == 'string') {
+        fieldString += `\nkeccak256(bytes(params.${field.name}))`;
+      } else {
+        fieldString += `\nparams.${field.name}`;
+      }
+    } else if (dataType.type == 'ArrayTypeName') {
+      fieldString += `\nabi.encodePacked(params.${field.name})`;
+    }
+    if (i < struct.fields.length - 1) {
+      fieldString += `,`;
+    }
+    console.log(field);
+  }
+  const resultString = `
+  function recover${structName}(${structName} params) external view returns (address) {
+    bytes32 structHash = keccak256(
+      abi.encode(
+        ${struct.typehash},${fieldString}
+      )
+    );
+  }`;
 
+  return resultString;
+};
