206c6a12aae68e9d5335a7e89989cce33b0233b2
Dmytro Sotnyk <d3magexgod@gmail.com>
2022-12-18 15:35:22 +0200

    todo

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,6 +1,7 @@
 import { parse } from '@solidity-parser/parser';
 import { ScrappedStruct, InitialStruct } from '../types';
-
+//todo: preprocess some of the data here to lessen amount of same for loop iterations in different files
+//i think i can make this generate all the necessary data whereas all the other file just arrange it in a proper way 
 export const parseStructs = (file: string): ScrappedStruct[] => {
   const children = parse(file).children.slice(1); //remove pragma
   const structs: InitialStruct[] = children as unknown as InitialStruct[];
