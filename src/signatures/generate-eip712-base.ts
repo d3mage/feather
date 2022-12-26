ea3049e8edb5afd3cf95c1764f032588f42ff7b8
Dmytro Sotnyk <d3magexgod@gmail.com>
2022-12-26 04:01:43 +0200

    enchanced eip712 base

--- src/signatures/generate-eip712-base.ts
+++ src/signatures/generate-eip712-base.ts
@@ -11,43 +11,54 @@ import "./Structs.sol";
 contract EIP712Base is EIP712 {
   constructor(string memory name, string memory version) EIP712 (name, version) {}`;
   for (const struct of structs) {
-    resultString += generateSingleFunction(struct);
+    resultString += generateHashFunction(struct);
+    resultString += generateRecoverFunction(struct);
   }
   resultString += `
 }`;
   return resultString;
 };
 
-const generateSingleFunction = (struct: ScrappedStruct): string => {
-  const structName = struct.structName;
-  console.log(struct);
+const generateHashFunction = (struct: ScrappedStruct): string => {
+  const structName = struct.name;
   let fieldString = '';
   for (let i = 0; i < struct.fields.length; ++i) {
     const field = struct.fields[i];
     const dataType = field.dataType;
-    //todo: check all elementary types
     fieldString += `\n\t\t\t\t`;
     if (dataType.type == 'ElementaryTypeName') {
       if (dataType.name == 'string') {
         fieldString += `keccak256(bytes(params.${field.name}))`;
+      } else if (dataType.name == 'bytes') {
+        fieldString += `keccak256(params.${field.name})`;
       } else {
         fieldString += `params.${field.name}`;
       }
     } else if (dataType.type == 'ArrayTypeName') {
       fieldString += `abi.encodePacked(params.${field.name})`;
+    } else if (dataType.type == 'UserDefinedTypeName') {
+      fieldString += `hash${dataType.namePath}(params.${field.name})`;
     }
     if (i < struct.fields.length - 1) {
       fieldString += `,`;
     }
-    console.log(field);
   }
   const resultString = `
-  function recover${structName}(${structName} calldata params, bytes memory signature) external view returns (address) {
-    bytes32 structHash = keccak256(
+function hash${structName}(${structName} calldata params) internal pure returns (bytes32) {
+    return keccak256(
       abi.encode(
         ${struct.typehash},${fieldString}
       )
     );
+  }`;
+  return resultString;
+};
+
+const generateRecoverFunction = (struct: ScrappedStruct): string => {
+  const structName = struct.name;
+  const resultString = `
+  function recover${structName}(${structName} calldata params, bytes memory signature) external view returns (address) {
+    bytes32 structHash = hash${structName}(params);
     bytes32 typedHash = _hashTypedDataV4(structHash);
     return ECDSA.recover(typedHash, signature);
   }`;
