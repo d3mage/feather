5df265f7b20052d63b2e6d2c4f729e507db00261
Dmytro Sotnyk <d3magexgod@gmail.com>
2022-12-27 19:43:54 +0200

    removed sign message

--- src/signatures/index.ts
+++ src/signatures/index.ts
@@ -1,6 +1,5 @@
 export * from './generate-types';
 export * from './generate-eip712-typehashes';
-export * from './generate-sign-message';
 export * from './generate-eip712-base';
 export * from './generate-test';
 export * from './shared';
